6194a235542ce2dfdababd6c19ec6c0e3b5652fd
Igor T <igor.tanv@gmail.com>
2025-02-07 13:49:00 +0700

    fixed navbar menu

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,6 +5,10 @@ import Link from "next/link";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className="bg-black text-white fixed top-0 left-0 w-full z-50 shadow-lg">
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
@@ -33,10 +37,10 @@ const Navbar = () => {
       {/* Mobile Menu */}
       {isOpen && (
         <div className="md:hidden bg-black flex flex-col space-y-4 p-4">
-          <Link href="/" className="hover:text-gray-400">Home</Link>
-          <Link href="/about" className="hover:text-gray-400">About</Link>
-          <Link href="/products" className="hover:text-gray-400">Products</Link>
-          <Link href="/contact" className="hover:text-gray-400">Contact</Link>
+          <Link href="/" className="hover:text-gray-400" onClick={handleLinkClick}>Home</Link>
+          <Link href="/#about" className="hover:text-gray-400" onClick={handleLinkClick}>About</Link>
+          <Link href="/#products" className="hover:text-gray-400" onClick={handleLinkClick}>Products</Link>
+          <Link href="/#contact" className="hover:text-gray-400" onClick={handleLinkClick}>Contact</Link>
         </div>
       )}
     </nav>
